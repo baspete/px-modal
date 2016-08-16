e1e6c74db2209ac4856f9f18e1c50c965147cfc3
Pete Butler <pete.butler@ge.com>
2016-08-15 19:00:17 -0700

    added -u and using default root

--- cdnify.js
+++ cdnify.js
@@ -3,36 +3,37 @@
 // changing any strings defined in the config section.
 //
 // Usage:
-//   node cdnify.js -v <version> (-d)
+//   node cdnify.js -v <version> (-d -u)
 //
 // Options:
 //   -v (string) Version. Required.
+//   -u (null)   Update dist files with CDN strings. Optional
 //   -d (null)   Dry run. Does everything but upload to the CDN. Optional
 //
 // Dependencies:
 //   process.env.AWS_ACCESS_KEY
 //   process.env.AWS_SECRET_ACCESS_KEY
-//   process.env.HTTP_PROXY
+//   process.env.HTTP_PROXY (optional - use if you're inside the 3. network)
 
-var argv = require('minimist')(process.argv.slice(2)), // get options passed to this program
+var args = require('minimist')(process.argv.slice(2), {'--':true,alias:{v:'version',d:'dryrun',u:'update'}}),
     fs = require('fs-extra'),
     cdn = require('../predix-cdn/');
 
 // #######################################################
 // CONFIGURATION
 
-    // Get the app name from package.json. This must be unique!
+// Get the app name from package.json. This must be unique!
 var appName = fs.readJsonSync('./package.json').name,
-    version = argv.v, // -v
-    dryrun = argv.d;  // -d
+    version = args.v, // -v or --version
+    dryrun = args.d,  // -d or --dryrun
+    update = args.u;  // -u or --update
 
 var options = {
   name: 'predixdev/' + appName,
   // Options passed when calling this program
   version: version,
   dryrun: dryrun,
-  // Where do we read files from?
-  root: './',
+  update: update,
   // Which files in there do we want to upload to the CDN?
   files: [
     'css/px-modal.css',
