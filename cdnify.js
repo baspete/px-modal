d7c1782208cc72be1489ed418ebe0522f9b1521d
Pete Butler <pete.butler@ge.com>
2016-08-16 18:52:20 -0700

    simplified config

--- cdnify.js
+++ cdnify.js
@@ -3,54 +3,52 @@
 // changing any strings defined in the config section.
 //
 // Usage:
-//   node cdnify.js -v <version> (-d -u)
+//   node cdnify.js (-v <version>) (-d)
 //
 // Options:
-//   -v (string) Version. Required.
-//   -u (null)   Update dist files with CDN strings. Optional
-//   -d (null)   Dry run. Does everything but upload to the CDN. Optional
+//   -v (string) Version (optional, package.json used if not provided).
+//   -d (null)   Dry run (optional). Does everything but upload to the CDN.
 //
 // Dependencies:
 //   process.env.AWS_ACCESS_KEY
 //   process.env.AWS_SECRET_ACCESS_KEY
-//   process.env.HTTP_PROXY (optional - use if you're inside the 3. network)
+//   process.env.HTTP_PROXY
 
-var args = require('minimist')(process.argv.slice(2), {'--':true,alias:{v:'version',d:'dryrun',u:'update'}}),
+var args = require('minimist')(process.argv.slice(2), {'--':true,alias:{v:'version',d:'dryrun'}}),
     fs = require('fs-extra'),
     cdn = require('../predix-cdn/');
 
 // #######################################################
 // CONFIGURATION
 
-// Get the app name from package.json. This must be unique!
-var appName = fs.readJsonSync('./package.json').name,
-    version = args.v, // -v or --version
-    dryrun = args.d,  // -d or --dryrun
-    update = args.u;  // -u or --update
-
 var options = {
-  name: 'predixdev/' + appName,
-  // Options passed when calling this program
-  version: version,
-  dryrun: dryrun,
-  update: update,
+  name: 'predixdev/' + fs.readJsonSync('./package.json').name,
+  version: args.v || fs.readJsonSync('./package.json').version,
+  dryrun: args.d,
+  update: false, // never change files that don't get uploaded.
   // Which files in there do we want to upload to the CDN?
   files: [
     'css/px-modal.css',
     'px-modal.html'
   ],
   // Which strings do we want to replace?
-  strings: [
-    {
-      file: '/polymer/polymer.html',
-      package: 'polymer',
-      cdn: '//polygit.org/polymer+:${version}/components/polymer/polymer.html'
-    }
+  // (predix-cdn will automatically replace polymer.html, es6-promise and webcomponents-lite)
+  strings: [],
+  // Which Px components do you depend on?
+  px: ['px-theme'],
+  // Ignore files in these directories
+  excludeDir: [
+    '.github',
+    'bower_components',
+    'node_modules',
+    'sass',
+    'scripts',
+    'test'
   ]
 };
 
 // END CONFIGURATION
 // #######################################################
 
-// CDNify everything in `root` and upload those files
+// CDNify and upload the files in options.files
 cdn.cdnify(options);
